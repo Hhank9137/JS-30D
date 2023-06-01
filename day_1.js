9d79f2c2874df907494f76e991357802342d4488
Hhank9137 <hank042698168@gmail.com>
2023-06-01 16:27:14 +0800

    缺少刪除效果的function(未完成)

--- day_1.js
+++ day_1.js
@@ -4,15 +4,28 @@
 
     function playHandler(e){
         console.log(e.keyCode)
-
-        
         // play music
 
         const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+        if(audio) {
+            audio.currentTime = 0;
+            audio.play(audio);
+        };
+        // 如果audio存在了話，執行播放對映到的編號
+
 
         // dom style
+
         const dom = document.querySelector(`div[data-key="${e.keyCode}"]`);
-        console.log(audio);
-       
+        if(dom) {
+            dom.classList.add('playing');
+        };
+        // 如果dom存在了話，執行在data key的class 加上playing
+
+        console.log(audio,dom);
     }
+    
+
+  
+
 })()
