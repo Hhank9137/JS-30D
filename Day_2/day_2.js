e2200ea4a839de1a250f991d5247fdc70e141363
Hhank9137 <hank042698168@gmail.com>
2023-06-06 00:30:43 +0800

    Day_2完成

--- Day_2/day_2.js
+++ Day_2/day_2.js
@@ -2,14 +2,16 @@
     const hour = document.querySelector('.hour-hand')
     const min = document.querySelector('.min-hand')
     const second = document.querySelector('.second-hand')
+    const divDate = document.querySelector('.date')
     // console.log(hour, min, second)
 
-    function setClock(){
+    function setClock(){ 
         const date = new Date()
-
+        console.log(date)
+        divDate.innerHTML = date;
         let secondDeg = date.getSeconds() * 6 // (360/60)
-        let minDeg = date.getMinutes() * 6 // (360/60)
-        let hourDeg = date.getHours() * 30 // (360/12)
+        let minDeg = date.getMinutes() * 6 + date.getSeconds() * 6 / 60 // (360/60)
+        let hourDeg = date.getHours() * 30 + date.getMinutes() * 30 / 60 // (360/12)、分鐘乘３０度，再除(每小時60分)
         // console.log(secondDeg)
 
         second.style.transform = `rotate(${secondDeg}deg)`;
@@ -17,6 +19,7 @@
         hour.style.transform = `rotate(${hourDeg}deg)`;
 
     }
-    setClock() //初始化畫面
-    setInterval(setClock,1000)
+    
+    // setClock() //初始化畫面
+    setInterval(setClock,1000)  //把setClock()函示，以1000毫秒(1秒)執行一次（持續執行）
 })()
