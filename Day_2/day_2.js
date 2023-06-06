41536a6fe6ca4e8076519f1bd2c23d55fce7b085
Hhank9137 <hank042698168@gmail.com>
2023-06-06 16:28:06 +0800

    Day_3 未完成(datasizing)

--- Day_2/day_2.js
+++ Day_2/day_2.js
@@ -3,6 +3,7 @@
     const min = document.querySelector('.min-hand')
     const second = document.querySelector('.second-hand')
     const divDate = document.querySelector('.date')
+    let strH = ''
     // console.log(hour, min, second)
 
     function setClock(){ 
@@ -13,7 +14,7 @@
         let minDeg = date.getMinutes() * 6 + date.getSeconds() * 6 / 60 // (360/60)
         let hourDeg = date.getHours() * 30 + date.getMinutes() * 30 / 60 // (360/12)、分鐘乘３０度，再除(每小時60分)
         // console.log(secondDeg)
-
+        
         second.style.transform = `rotate(${secondDeg}deg)`;
         min.style.transform = `rotate(${minDeg}deg)`;
         hour.style.transform = `rotate(${hourDeg}deg)`;
@@ -22,4 +23,5 @@
     
     // setClock() //初始化畫面
     setInterval(setClock,1000)  //把setClock()函示，以1000毫秒(1秒)執行一次（持續執行）
+    
 })()
