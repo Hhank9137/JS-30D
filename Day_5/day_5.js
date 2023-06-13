00151e7a7bf72132e9744a6955beda78cb1df424
Hhank9137 <hank042698168@gmail.com>
2023-06-13 15:21:29 +0800

    Day_5完成23/06/13

--- Day_5/day_5.js
+++ Day_5/day_5.js
@@ -1,17 +1,33 @@
-(function() {
+;(function() {
     const panels = document.querySelectorAll('.panel');
     console.log(panels)
     
 
-    function clickHandler(){
-        this.classList.toggle("open");
-    }
     
+
+    function clickHandler(){
+        this.classList.toggle('open');
+    }
+
+    function transitionHandler(e){
+        console.log(e)
+        console.log(e.propertyName.indexOf('flex'))
+        
+        if(e.propertyName.indexOf('flex')!== -1){
+            this.classList.toggle('open-active');
+        }
+        
+
+    }
     // JQ: addClass / removeclass / toggleClass
     // JS: classList.add / classList.remove / classList.toggle  
 
     panels.forEach((panel) =>{
-        panel.addEventListener('click',clickHandler());
-        
+        panel.addEventListener('click', clickHandler);
+        // 監聽點擊事件觸發事件(P_1)
+        panel.addEventListener('transitionend', transitionHandler)
+        // 監聽動畫結束觸發事件(P_2)
+        // 
     })
-})
+    
+})()
